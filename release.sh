74d44351d55f71b78b88d789572f966bf9283d83
Simon Reinisch <simon@reinisch.io>
2020-02-03 14:31:44 +0100

    Add icon and update release.sh

--- release.sh
+++ release.sh
@@ -1,10 +1,28 @@
 #!/usr/bin/env bash
 
-
 # Build project
-cargo clean
 cargo build --release --bin cleanup
 wait
+
+# Add executable icon on windows platforms
+if [[ "$(expr substr "$(uname)" 1 5)" == "MINGW" ]]; then
+  rcedit_path="./target/rcedit.exe"
+  echo "Detected windows platform."
+
+  # Download rcedit if not already done so
+  if [[ ! -f "$($rcedit_path)" ]]; then
+    echo "Downloading rcedit..."
+    curl -L -s -o "$($rcedit_path)" "https://github.com/electron/rcedit/releases/download/v1.1.1/rcedit-x64.exe"
+  else
+    echo "Found rcedit!"
+  fi
+
+  # Add icon
+  echo "Adding icon..."
+  $rcedit_path ./target/release/cleanup.exe --set-icon icon.ico
+
+  echo "Icon successfully added!"
+fi
 
 # Target file
 path=./target/release/cleanup.exe
@@ -33,6 +51,6 @@ SHA-256: $sha256
 SHA-512: $sha512
 "
 
-echo "$content" >> "$destfile"
+echo "$content" >>"$destfile"
 echo "$content"
 echo "Saved to $destfile"
