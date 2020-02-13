4ed2d802dfbf6a9f39b4b32a5a10aa9a0026a2eb
Simon <simon@reinisch.io>
2020-02-13 09:24:43 +0100

    Add more tests

--- tests/cli.rs
+++ tests/cli.rs
@@ -5,7 +5,7 @@ mod lib;
 #[test]
 fn simple_sort() {
     lib::test_command(|cmd| {
-        cmd.arg(".").unwrap();
+        cmd.arg(".").assert().success();
 
         vec![
             "misc/txt/t1.txt",
@@ -22,7 +22,7 @@ fn simple_sort() {
 #[test]
 fn custom_target() {
     lib::test_command(|cmd| {
-        cmd.arg(".").arg("sorted").unwrap();
+        cmd.arg(".").arg("sorted").assert().success();
 
         vec![
             "sorted/txt/t1.txt",
@@ -33,5 +33,100 @@ fn custom_target() {
             "sorted/psd/f2.psd",
             ".ignored-file",
         ]
+    });
+}
+
+#[test]
+fn exclude_extensions() {
+    lib::test_command(|cmd| {
+        cmd.arg(".").arg("--ext").arg("txt,mp4").assert().success();
+
+        vec![
+            "misc/psd/f1.psd",
+            "misc/psd/f2.psd",
+            "t1.txt",
+            "t2.txt",
+            "m1.mp4",
+            "m2.mp4",
+            ".ignored-file",
+        ]
+    });
+}
+
+#[test]
+fn dry_run() {
+    lib::test_command(|cmd| {
+        cmd.arg(".")
+            .arg("--ext")
+            .arg("txt,mp4")
+            .arg("-d")
+            .assert()
+            .success();
+
+        vec![
+            "f1.psd",
+            "f2.psd",
+            "t1.txt",
+            "t2.txt",
+            "m1.mp4",
+            "m2.mp4",
+            ".ignored-file",
+        ]
+    });
+}
+
+#[test]
+fn version_help() {
+    lib::test_command(|cmd| {
+        cmd.arg(".")
+            .arg("-v")
+            .assert()
+            .success()
+            .stdout(predicates::str::contains(format!(
+                "v{}",
+                env!("CARGO_PKG_VERSION")
+            )));
+
+        // Nothing should change
+        vec![
+            "f1.psd",
+            "f2.psd",
+            "t1.txt",
+            "t2.txt",
+            "m1.mp4",
+            "m2.mp4",
+            ".ignored-file",
+        ]
+    });
+}
+
+#[test]
+fn invalid_source() {
+    lib::test_command(|cmd| {
+        cmd.arg("./bar/foo").assert().failure();
+
+        vec![]
+    });
+}
+
+#[test]
+fn log_file() {
+    lib::test_command(|cmd| {
+        cmd.arg(".").arg("-l").assert().success();
+
+        vec!["misc/cleanup.log"]
+    });
+}
+
+#[test]
+fn custom_log_file() {
+    lib::test_command(|cmd| {
+        cmd.arg(".")
+            .arg("--log-file")
+            .arg("hello.txt")
+            .assert()
+            .success();
+
+        vec!["misc/hello.txt"]
     });
 }
